4fbf5c20481bf021195711fd842fa9d91cfd3869
Ryland Stamey <rylandstamey@Rylands-MacBook-Pro.local>
2023-02-12 21:25:35 -0800

    added prompts and confirms to select char types and passcode length

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// Assignment code here
-var passwordLength = 10;
-const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-const charLength = charSet.length;
-// Get references to the #generate element
+
+// Get references to the #generate element*/
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,16 +7,47 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-console.log ('password', password)
-console.log (passwordText)
 }
-function generatePassword(){
-  var newPassword = ""
-    for (var i = 1; i <= passwordLength; i++) {
-   newPassword += charSet.charAt(Math.floor(Math.random() * charLength));
-   console.log ('newPASSWORD', newPassword)
+
+function generatePassword() {
+  // asks for password length
+  var passwordLength = prompt("Enter password length (minimum 8 characters and maximum 128 characters):");
+  // check if password length is ok
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters. try again.");
+    passwordLength = prompt("Enter password length (min 8 characters and max 128):");
   }
-  return newPassword
+  // ask what chars
+  var includeLowercase = confirm("Include lowercase characters in the password?");
+  var includeUppercase = confirm("Include uppercase characters in the password?");
+  var includeNumeric = confirm("Include number characters in the password?");
+  var includeSpecial = confirm("Include special characters in the password?");
+  // char choses
+   while (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
+    alert("You must select at least one character type. Please try again.");
+    includeLowercase = confirm("Include lowercase characters in the password?");
+    includeUppercase = confirm("Include uppercase characters in the password?");
+    includeNumbers = confirm("Include numeric characters in the password?");
+    includeSpecial = confirm("Include special characters in the password?");
+  }
+  // define chars
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var special = "!@#$%^&*()_+-=[]{}|;':<>,.?/";
+  // combine characters
+  var characterSet = "";
+  if (includeLowercase) { characterSet += lowercase; }
+  if (includeUppercase) { characterSet += uppercase; }
+  if (includeNumeric) { characterSet += numbers; }
+  if (includeSpecial) { characterSet += special; }
+  // generate the password
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    password += characterSet.charAt(Math.floor(Math.random() * characterSet.length));
+  }
+  // return the password to be passed the window
+   return password;
 }
-// Add event listener to generate button
+// button event listener
 generateBtn.addEventListener("click", writePassword);
