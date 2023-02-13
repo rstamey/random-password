5de1b6abf455e3965c051a5dc5a54328f4e15d48
Ryland Stamey <rylandstamey@Rylands-MacBook-Pro.local>
2023-02-12 20:53:38 -0800

    add function that finds random 10 characters in a string and outputs it to the window

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment code here
-
-
+var passwordLength = 10;
+const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const charLength = charSet.length;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -8,10 +9,17 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+console.log ('password', password)
+console.log (passwordText)
 }
-
+function generatePassword(){
+  var newPassword = ""
+    for (var i = 1; i <= passwordLength; i++) {
+   newPassword += charSet.charAt(Math.floor(Math.random() * charLength));
+   console.log ('newPASSWORD', newPassword)
+  }
+  return newPassword
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
